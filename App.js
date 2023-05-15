9c8cc9034949e6f9caf8cf876a86c481bf77ed5b
Elsa Letallieur <elsa.c.letallieur@gmail.com>
2023-05-15 15:33:44 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import Constants from "expo-constants";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+import logo from "./assets/logo-imdb.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Image source={logo} style={styles.img} />
+      </View>
+      <Text style={styles.title}>Interstellar</Text>
+      <View style={styles.mainInfo}>
+        <Text style={styles.mainInfoText}>2014</Text>
+        <Text style={styles.mainInfoText}>PG-13</Text>
+        <Text style={styles.mainInfoText}>2h 49min</Text>
+        <Text style={styles.mainInfoText}>Adventure, Drama, Sci-Fi</Text>
+      </View>
+      <Text>we need many many beers</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    color: "white",
+    // alignItems: "center",
+    // justifyContent: "center",
+    marginTop: Constants.statusBarHeight,
+  },
+  header: {
+    height: 80,
+    backgroundColor: "grey",
+    justifyContent: "center",
+  },
+  img: {
+    height: 40,
+    width: 110,
+    // transform: [{ scale: 0.55 }],
+    marginLeft: 4,
+  },
+  title: {
+    fontSize: 30,
+    color: "white",
+    marginBottom: 10,
+  },
+  mainInfo: {
+    flexDirection: "row",
+  },
+  mainInfoText: {
+    color: "white",
+    marginLeft: 8,
   },
 });
